22ada0e67883cc53ded460ec7a1c9a4f36a629ef
nitamanna <47340367+nitamanna@users.noreply.github.com>
2019-05-29 23:39:23 +0530

    update keypair name in main.tf

--- module/key-pair/main.tf
+++ module/key-pair/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "my-terra-kp" {
-  key_name = "my-terra-kp"
+  key_name = "my-terra-kp2"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCXhK3mfOWCUDpp9lmb5vnI+SGdfDqS0WSlOonXMv5zwmqM12MDYiM4K8A5nohaMxkqrLygVzWdUfVsP0x1FR0E10hcDUrAsH8NobemUAUg4igJahdH7DnKsHAYtxjMmcBWvec4lo9x4b9K0eoamAYl5WeLknJuOPy8XlJV1WyCyYtPEVHmkEJy4h50x9qQwz8qmCQVV/i32h7UB94gtqP5LM3Ut6gg+q2wyIMDpDM3csCNkk8haKH3SB+EhdxU1KNyugbnL2E+i5SHLYL5DHkxXtPlQp0JDSjyNder3HwzcbVsIgP1nugs7FKknulHuj86u+speLtvj/Ng7+xEMcEB nm-ec2-keypair01"
 }
